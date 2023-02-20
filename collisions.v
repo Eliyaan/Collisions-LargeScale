e2f5e468bcb6b9cbc7a9e0966444f975eccdc0a5
Eliyaan <103932369+Eliyaan@users.noreply.github.com>
2023-02-20 22:30:52 +0100

    Update collisions.v

--- collisions.v
+++ collisions.v
@@ -20,7 +20,6 @@ struct Particle{
 	old_y f64
 	acc_x f64
 	acc_y f64
-	opti_index int
 }
 
 
@@ -113,42 +112,29 @@ fn main() {
 fn on_frame(mut app App) {
     app.gg.begin()
 	for mut parti in app.list_parti{
-		println(parti.opti_index)
-		app.list_opti[int(parti.y/app.parti_size)][int(parti.x/app.parti_size)].delete(parti.opti_index)
-		for otheri in parti.opti_index..app.list_opti[int(parti.y/app.parti_size)][int(parti.x/app.parti_size)].len{
-			app.list_opti[int(parti.y/app.parti_size)][int(parti.x/app.parti_size)][otheri].opti_index -= 1
-		}
 		parti.accelerate(0, 0.5)
 		parti.update_pos()
+		parti.correct_constraints()
 		mut delta_x := 0.0
 		mut delta_y := 0.0
-		for i in -1..2{
-			y_index := int(parti.y/app.parti_size) + i
-			if y_index >= 0 && y_index < int(win_height/app.parti_size){
-				for j in -1..2{
-					x_index := int(parti.x/app.parti_size) + j
-					if x_index >= 0 && x_index < int(win_width/app.parti_size){
-						for oi ,mut other in app.list_opti[y_index][x_index]{
-							diff_x := parti.x - other.x
-							diff_y := parti.y - other.y
-							if diff_x < app.sqtwo_radius || diff_y < app.sqtwo_radius{
-								delta_x += diff_x/2
-								delta_y += diff_y/2  //Apply it to the other one ?
-							}else if diff_x*diff_x + diff_y*diff_y < app.pow_radius{
-								delta_x += diff_x/2
-								delta_y += diff_y/2  //Apply it to the other one ?
-							}						
-						}
-					}
-				}
-			}
+		for other in app.list_parti{
+			diff_x := parti.x - other.x
+			diff_y := parti.y - other.y
+			if diff_x < app.sqtwo_radius || diff_y < app.sqtwo_radius{
+				println(diff_x)
+				println(diff_y)
+				delta_x += diff_x/2
+				delta_y += diff_y/2  //Apply it to the other one ?
+			}else if diff_x*diff_x + diff_y*diff_y < app.pow_radius{
+				delta_x += diff_x/2
+				delta_y += diff_y/2  //Apply it to the other one ?
+			}						
 		}
+
 		parti.x += delta_x
 		parti.y += delta_y
 		parti.correct_constraints()
 		app.gg.draw_circle_filled(f32(parti.x), f32(parti.y), app.parti_size, gx.white)
-		parti.opti_index = app.list_opti[int(parti.y/app.parti_size)][int(parti.x/app.parti_size)].len
-		app.list_opti[int(parti.y/app.parti_size)][int(parti.x/app.parti_size)] << &parti
 	}
     //Draw
     app.gg.end()
@@ -175,7 +161,6 @@ fn on_event(e &gg.Event, mut app App){
 
 fn (mut app App) spawn_parti(x f32, y f32){
 	if x < win_width && y < win_height{
-		app.list_parti << Particle{x, y, app.parti_size, x, y, 0, 0, app.list_opti[int(y/app.parti_size)][int(x/app.parti_size)].len}
-		app.list_opti[int(y/app.parti_size)][int(x/app.parti_size)] << &(app.list_parti[app.list_parti.len -1])
+		app.list_parti << Particle{x, y, app.parti_size, x, y, 0, 0}
 	}
 }
