60c914cbc0629bbcd27efa8d2b9dcbb5cad24da0
Eliyaan <103932369+Eliyaan@users.noreply.github.com>
2023-02-22 23:13:51 +0100

    working great ^^

--- collisions.v
+++ collisions.v
@@ -11,6 +11,7 @@ const (
 	dt = 0.016
 	big_circle_radius = 300
 	big_circle_pos = 300
+	text_cfg = gx.TextCfg{color: gx.white, size: 20, align: .left, vertical_align: .top}
 )
 
 [heap]
@@ -28,15 +29,15 @@ struct Particle{
 }
 
 
-fn (mut parti Particle) update_pos(){
+fn (mut parti Particle) update_pos(delta_time f64){
 	velocity_x := parti.x - parti.old_x
 	velocity_y := parti.y - parti.old_y
 
 	parti.old_x = parti.x
 	parti.old_y = parti.y
 
-	parti.x = parti.x + velocity_x + parti.acc_x * dt * dt
-	parti.y = parti.y + velocity_y + parti.acc_y * dt * dt
+	parti.x = parti.x + velocity_x + parti.acc_x * delta_time * delta_time
+	parti.y = parti.y + velocity_y + parti.acc_y * delta_time * delta_time
 
 }
 
@@ -82,7 +83,7 @@ mut:
 
 	list_parti []Particle
 	list_opti [][][]&Particle
-	parti_size int = 16
+	parti_size int = 12
 	pow_radius int
 }
 
@@ -118,23 +119,55 @@ fn main() {
 
 
 
+fn (mut app App) solve_collisions(){
+	response_coef := 1.0
+	for mut parti in app.list_parti{
+		for mut other in app.list_parti{
+			if parti != other{
+				dist_x := parti.x - other.x
+				dist_y := parti.y - other.y
+                mut dist := dist_x * dist_x + dist_y * dist_y
+                min_dist := parti.radius + other.radius
+                // Check overlapping
+                if dist < min_dist * min_dist {
+                    dist  = m.sqrt(dist)
+                    n_x := dist_x / dist
+					n_y := dist_y / dist
+					delta := 0.5 * response_coef * (dist - min_dist)
+                    mass_ratio_a := (f64(parti.radius) / min_dist) * delta  //not just mass ratio
+                    mass_ratio_b := (f64(other.radius) / min_dist) * delta
+                    // Update positions
+                    parti.x -= n_x * (mass_ratio_b)
+					parti.y -= n_y * (mass_ratio_b)
+                    other.x += n_x * (mass_ratio_a)
+					other.y += n_y * (mass_ratio_a)
+                }
+			}
+		}
+	}
+}
 
 
 fn on_frame(mut app App) {
-	for mut parti in app.list_parti{
-		parti.accelerate(0, 10000)
-	}
-	for mut parti in app.list_parti{
-		parti.update_pos()
-	}
-	for mut parti in app.list_parti{
-		parti.correct_constraints_circle()
+	sub := 6.0
+	for i in 0..int(sub){
+		for mut parti in app.list_parti{
+			parti.accelerate(0, 10000/sub)
+		}
+		for mut parti in app.list_parti{
+			parti.update_pos(dt/sub)
+		}
+		app.solve_collisions()
+		for mut parti in app.list_parti{
+			parti.correct_constraints_circle()
+		}
 	}
     //Draw
 	app.gg.begin()
-	for mut parti in app.list_parti{
-		app.gg.draw_circle_filled(f32(parti.x), f32(parti.y), app.parti_size, gx.white)
+	for parti in app.list_parti{
+		app.gg.draw_circle_filled(f32(parti.x), f32(parti.y), parti.radius, gx.Color{u8(parti.radius*8%255),u8(parti.radius*16%255),u8(parti.radius*64%255), 255})
 	}
+	app.gg.draw_text(840, 55, "Nb particles' size: ${app.list_parti.len}", text_cfg)
     app.gg.end()
 }
 
@@ -159,6 +192,10 @@ fn on_event(e &gg.Event, mut app App){
 
 fn (mut app App) spawn_parti(x f32, y f32){
 	if x < win_width && y < win_height{
-		app.list_parti << Particle{x, y, app.parti_size, x, y, 0, 0, 0, 0}
+		app.list_parti << Particle{x, y, rd.int_in_range(3, 15) or {12}, x, y, 0, 0, 0, 0}
+		app.list_parti << Particle{x+1, y+1, rd.int_in_range(3, 15) or {12}, x, y, 0, 0, 0, 0}
+		app.list_parti << Particle{x+2, y+3, rd.int_in_range(3, 15) or {12}, x, y, 0, 0, 0, 0}
+		app.list_parti << Particle{x+5, y+5, rd.int_in_range(3, 15) or {12}, x, y, 0, 0, 0, 0}
+
 	}
 }
