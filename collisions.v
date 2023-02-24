599317c22e20f35668fdb15dcabef1cf034e29f5
Eliyaan <103932369+Eliyaan@users.noreply.github.com>
2023-02-24 21:27:10 +0100

    Squashed commit of the following:
    commit 8a8d4d3d0d5fdf79153537386a9f0973817ed885
    Author: Eliyaan <103932369+Eliyaan@users.noreply.github.com>
    Date:   Fri Feb 24 21:26:31 2023 +0100
    
        Update collisions.v
    
    commit 3968c1985fb129aea1d0dfc7fbd2c724094ccf12
    Author: Eliyaan <103932369+Eliyaan@users.noreply.github.com>
    Date:   Thu Feb 23 23:29:16 2023 +0100
    
        Update collisions.v
    
    commit f28f7b1af90c955e85730ad2e655187913356524
    Author: Eliyaan <103932369+Eliyaan@users.noreply.github.com>
    Date:   Thu Feb 23 12:45:54 2023 +0100
    
        opti wip
    
    commit aa7f072d0367e9eaff33e323f828d7f0b43bc958
    Author: Eliyaan <103932369+Eliyaan@users.noreply.github.com>
    Date:   Thu Feb 23 00:00:20 2023 +0100
    
        Update collisions.v
    
    commit 60c914cbc0629bbcd27efa8d2b9dcbb5cad24da0
    Author: Eliyaan <103932369+Eliyaan@users.noreply.github.com>
    Date:   Wed Feb 22 23:13:51 2023 +0100
    
        working great ^^
    
    commit 3e9e7b735a52397cb604e5b61a21ca2f1a7d33c9
    Author: Eliyaan <103932369+Eliyaan@users.noreply.github.com>
    Date:   Tue Feb 21 20:49:22 2023 +0100
    
        Update collisions.v
    
    commit c3baa2ceded881929d10941e947bb1e82a278b76
    Author: Eliyaan <103932369+Eliyaan@users.noreply.github.com>
    Date:   Tue Feb 21 16:54:03 2023 +0100
    
        working circlebox
    
    commit 3af6fd249953ba1cf5b351c407c8b1a7d044514c
    Author: Eliyaan <103932369+Eliyaan@users.noreply.github.com>
    Date:   Mon Feb 20 23:40:37 2023 +0100
    
        jsp x)
    
    commit d4a299fef46fb3188e0c77abb4f88c6ed9447532
    Author: Eliyaan <103932369+Eliyaan@users.noreply.github.com>
    Date:   Mon Feb 20 23:23:07 2023 +0100
    
        Update collisions.v
    
    commit e2f5e468bcb6b9cbc7a9e0966444f975eccdc0a5
    Author: Eliyaan <103932369+Eliyaan@users.noreply.github.com>
    Date:   Mon Feb 20 22:30:52 2023 +0100
    
        Update collisions.v

--- collisions.v
+++ collisions.v
@@ -3,11 +3,18 @@ import gg
 import gx
 import rand as rd
 import math as m
+import time
 
 const (
     win_width    = 600
     win_height   = 600
-    bg_color     = gx.black
+    bg_color     = gx.white
+	dt = 0.016
+	big_circle_radius = 300
+	big_circle_pos = 300
+	text_cfg = gx.TextCfg{color: gx.black, size: 20, align: .left, vertical_align: .top}
+	response_coef = 1.0
+	half_response_coef = response_coef * 0.5
 )
 
 [heap]
@@ -16,43 +23,55 @@ struct Particle{
     x f64
     y f64
 	radius int
+	fradius f32
 	old_x f64
 	old_y f64
 	acc_x f64
 	acc_y f64
-	opti_index int
+	delta_x f64
+	delta_y f64
+	pression f64
 }
 
 
-fn (mut parti Particle) update_pos(){
+fn (mut parti Particle) update_pos(delta_time f64){
 	velocity_x := parti.x - parti.old_x
 	velocity_y := parti.y - parti.old_y
 
 	parti.old_x = parti.x
 	parti.old_y = parti.y
 
-	parti.x = parti.x + velocity_x + parti.acc_x 
-	parti.y = parti.y + velocity_y + parti.acc_y
-
-	parti.correct_constraints()
+	parti.x = parti.x + velocity_x + parti.acc_x * delta_time * delta_time
+	parti.y = parti.y + velocity_y + parti.acc_y * delta_time * delta_time
 }
 
 
-fn (mut parti Particle) correct_constraints(){
+fn (mut parti Particle) correct_constraints_square(){
 	if parti.y + parti.radius > win_height{
 		parti.y += win_height - (parti.y+parti.radius)
-		parti.old_y = parti.y
 	}else if parti.y - parti.radius < 0{
 		parti.y += -(parti.y-parti.radius)
-		parti.old_y = parti.y
 	}
 	if parti.x + parti.radius> win_width{
 		parti.x += win_width - (parti.x+parti.radius)
-		parti.old_x = parti.x
 	}
 	else if parti.x - parti.radius < 0{
 		parti.x += -(parti.x-parti.radius)
-		parti.old_x = parti.x
+	}
+}
+
+
+fn (mut parti Particle) correct_constraints_circle(){
+	to_obj_x := big_circle_pos - parti.x 
+	to_obj_y := big_circle_pos - parti.y
+	mut dist := to_obj_x*to_obj_x+to_obj_y*to_obj_y
+	normal_dist := big_circle_radius - parti.radius
+	if dist > normal_dist*normal_dist{
+		dist= m.sqrt(dist)
+		n_x := to_obj_x/dist
+		n_y := to_obj_y/dist
+		parti.x = big_circle_pos - n_x * normal_dist
+		parti.y = big_circle_pos - n_y * normal_dist//thalès
 	}
 }
 
@@ -62,7 +81,6 @@ fn (mut parti Particle) accelerate(new_acc_x f64, new_acc_y f64){
 	parti.acc_y = new_acc_y
 }
 
-
 [heap]
 struct App {
 mut:
@@ -70,16 +88,30 @@ mut:
 
 	list_parti []Particle
 	list_opti [][][]&Particle
-	parti_size int = 10
+	parti_size int = 12
 	pow_radius int
-	sqtwo_radius f64
+	fps_counter []time.Duration = []time.Duration{len: 30}
+	time_last_frame time.Time
+
+	pression_view bool
+	carre_circle bool 
+	mouse_x f32
+	mouse_y f32
+	mouse_pressed bool
+	substeps f32 = 8.0
+	red_factor int = -2
+	green_factor int = 16
+	blue_factor int = 64
+
+	min_parti_size int = 2
+	max_parti_size int = 16
 }
 
 
-fn (mut app App) create_opti_list(){
+/*fn (mut app App) create_opti_list(){
 	for mut parti in app.list_parti{
 	}
-}
+}*/
 
 
 fn main() {
@@ -100,59 +132,150 @@ fn main() {
     )
 
 	app.list_opti = [][][]&Particle{len:win_height/app.parti_size, init:[][]&Particle{len:win_width/app.parti_size, init:[]&Particle{cap:4}}}
-	app.pow_radius = app.parti_size*app.parti_size
-	app.sqtwo_radius = m.sqrt(2) * f64(app.parti_size)
+	app.pow_radius = (4*app.parti_size*app.parti_size)
     //lancement du programme/de la fenêtre
     app.gg.run()
 }
 
 
 
+fn (mut app App) solve_collisions(){
+	len := app.list_parti.len
+	for i in 0..len{
+		mut parti := &(app.list_parti[i])
+		for j := i+1; j < len; j+=1{
+			mut other := &(app.list_parti[j])
+			dist_x := parti.x - other.x
+			dist_y := parti.y - other.y
+			mut dist := dist_x * dist_x + dist_y * dist_y
+			min_dist := parti.radius + other.radius
+			// Check overlapping
+			if dist < min_dist * min_dist && dist >= 1{
+				dist  = m.sqrt(dist)
+				n_x := dist_x / dist
+				n_y := dist_y / dist
+				delta := half_response_coef * (dist - min_dist)
+				mass_ratio_a := (parti.fradius / min_dist) * delta  //not just mass ratio
+				mass_ratio_b := (other.fradius / min_dist) * delta
+				// Update positions
+				xb := n_x * (mass_ratio_b)
+				yb := n_y * (mass_ratio_b)
+				xa := n_x * (mass_ratio_a)
+				ya := n_y * (mass_ratio_a)
+				parti.x -= xb
+				parti.y -= yb
+				other.x += xa
+				other.y += ya
+				if app.pression_view{
+					parti.pression += m.abs(xb + yb)
+					other.pression += m.abs(xa + ya)
+				}
+			}
+			
+		}
+	}
+}
 
 
 fn on_frame(mut app App) {
-    app.gg.begin()
-	for mut parti in app.list_parti{
-		println(parti.opti_index)
-		app.list_opti[int(parti.y/app.parti_size)][int(parti.x/app.parti_size)].delete(parti.opti_index)
-		for otheri in parti.opti_index..app.list_opti[int(parti.y/app.parti_size)][int(parti.x/app.parti_size)].len{
-			app.list_opti[int(parti.y/app.parti_size)][int(parti.x/app.parti_size)][otheri].opti_index -= 1
+	if app.mouse_pressed{
+		app.spawn_parti()
+		app.check_buttons()
+	}
+	for _ in 0..int(app.substeps){
+		for mut parti in app.list_parti{
+			parti.accelerate(0, 10000/app.substeps)
+			parti.pression = 0
 		}
-		parti.accelerate(0, 0.5)
-		parti.update_pos()
-		mut delta_x := 0.0
-		mut delta_y := 0.0
-		for i in -1..2{
-			y_index := int(parti.y/app.parti_size) + i
-			if y_index >= 0 && y_index < int(win_height/app.parti_size){
-				for j in -1..2{
-					x_index := int(parti.x/app.parti_size) + j
-					if x_index >= 0 && x_index < int(win_width/app.parti_size){
-						for oi ,mut other in app.list_opti[y_index][x_index]{
-							diff_x := parti.x - other.x
-							diff_y := parti.y - other.y
-							if diff_x*diff_x + diff_y*diff_y < app.pow_radius{
-								delta_x += diff_x/2
-								delta_y += diff_y/2  //Apply it to the other one ?
-							}						
-						}
-					}
-				}
+		for mut parti in app.list_parti{
+			parti.update_pos(dt/app.substeps)
+		}
+		app.solve_collisions()
+		if app.carre_circle{
+			for mut parti in app.list_parti{
+				parti.correct_constraints_circle()
+			}
+		}else{
+			for mut parti in app.list_parti{
+				parti.correct_constraints_square()
 			}
 		}
-		parti.x += delta_x
-		parti.y += delta_y
-		parti.correct_constraints()
-		app.gg.draw_circle_filled(f32(parti.x), f32(parti.y), app.parti_size, gx.white)
-		parti.opti_index = app.list_opti[int(parti.y/app.parti_size)][int(parti.x/app.parti_size)].len
-		app.list_opti[int(parti.y/app.parti_size)][int(parti.x/app.parti_size)] << &parti
 	}
     //Draw
+	app.gg.begin()
+	if app.carre_circle{
+		app.gg.draw_circle_filled(300, 300, 300, gx.black)
+	}else{
+		app.gg.draw_square_filled(0, 0, 600, gx.black)
+	}
+	if app.pression_view{
+		for parti in app.list_parti{
+			pression := parti.pression*82
+			if pression > 255{
+				app.gg.draw_circle_filled(f32(parti.x), f32(parti.y), parti.radius, gx.Color{163, 0, 38, 255})
+			}else{
+				app.gg.draw_circle_filled(f32(parti.x), f32(parti.y), parti.radius, gx.Color{255, 255-u8(pression), 255-u8(pression), 255})
+			}
+		}
+	}else{
+		for parti in app.list_parti{
+		app.gg.draw_circle_filled(f32(parti.x), f32(parti.y), parti.radius, gx.Color{u8(parti.radius*app.red_factor%255),u8(parti.radius*app.green_factor%255),u8(parti.radius*app.blue_factor%255), 255})
+		}
+	}
+
+	
+	mut fpstime := 0
+	for elem in app.fps_counter{
+		fpstime += elem
+	}
+	fpstime /= app.fps_counter.len
+	app.gg.draw_text(840, 585, "FPS: ${1000/(fpstime+1)}", text_cfg)
+	app.fps_counter.delete(0)
+	app.fps_counter << (time.now()-app.time_last_frame).milliseconds()
+	app.time_last_frame = time.now()
+
+	app.gg.draw_text(840, 555, "Nb particles: ${app.list_parti.len}", text_cfg)
+
+	app.gg.draw_text(840, 25, "Pression view: ", text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 26, 20, 20, 4,  gx.Color{255,182,193,255})
+
+	app.gg.draw_text(840, 55, "Square/Circle: ", text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 56, 20, 20, 4,  gx.Color{255,182,193,255})
+
+	app.gg.draw_text(840, 85, "Nb substeps: ${app.substeps}", text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 86, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255})
+	app.gg.draw_rounded_rect_filled(1070, 86, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255})
+
+	app.gg.draw_text(840, 115, "Reset the particles: ", text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 116, 20, 20, 4,  gx.Color{255,182,193,255})
+
+	app.gg.draw_text(840, 145, "Red Factor: ${app.red_factor}", text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 146, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255})
+	app.gg.draw_rounded_rect_filled(1070, 146, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255})
+
+	app.gg.draw_text(840, 175, "Green Factor: ${app.green_factor}", text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 176, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255})
+	app.gg.draw_rounded_rect_filled(1070, 176, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255})
+
+	app.gg.draw_text(840, 205, "Blue Factor: ${app.blue_factor}", text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 206, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255})
+	app.gg.draw_rounded_rect_filled(1070, 206, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255})
+
+	app.gg.draw_text(840, 235, "Min parti size: ${app.min_parti_size}", text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 236, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255})
+	app.gg.draw_rounded_rect_filled(1070, 236, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255})
+
+	app.gg.draw_text(840, 265, "Max parti size: ${app.max_parti_size}", text_cfg)
+    app.gg.draw_rounded_rect_filled(1040, 266, 20, 20, 4,  gx.Color{r: 230, g: 200, b: 255})
+	app.gg.draw_rounded_rect_filled(1070, 266, 20, 20, 4,  gx.Color{r: 255, g: 160, b: 255})
+
     app.gg.end()
 }
 
 
 fn on_event(e &gg.Event, mut app App){
+	app.mouse_x = e.mouse_x
+	app.mouse_y = e.mouse_y
     match e.typ {
         .key_down {
             match e.key_code {
@@ -162,7 +285,15 @@ fn on_event(e &gg.Event, mut app App){
         }
         .mouse_down {
             match e.mouse_button{
-                .left{app.spawn_parti(e.mouse_x, e.mouse_y)}
+                .left{app.mouse_pressed = true
+						}
+                else{}
+        }}
+		.mouse_up {
+            match e.mouse_button{
+                .left{
+						app.mouse_pressed = false
+						}
                 else{}
         }}
         else {}
@@ -170,9 +301,54 @@ fn on_event(e &gg.Event, mut app App){
 }
 
 
-fn (mut app App) spawn_parti(x f32, y f32){
-	if x < win_width && y < win_height{
-		app.list_parti << Particle{x, y, app.parti_size, x, y, 0, 0, app.list_opti[int(y/app.parti_size)][int(x/app.parti_size)].len}
-		app.list_opti[int(y/app.parti_size)][int(x/app.parti_size)] << &(app.list_parti[app.list_parti.len -1])
+fn (mut app App) spawn_parti(){
+	if app.mouse_x < win_width && app.mouse_y < win_height{
+		radius := rd.int_in_range(app.min_parti_size, app.max_parti_size) or {12}
+		app.list_parti << Particle{app.mouse_x, app.mouse_y, radius, f32(radius), app.mouse_x, app.mouse_y, 0, 0, 0, 0, 0}
 	}
+}
+
+
+fn (mut app App) check_buttons(){
+    if app.mouse_x > 1040 && app.mouse_x < 1090{
+        if app.mouse_x < 1060{
+            match true{
+                (app.mouse_y > 26 && app.mouse_y < 46){app.pression_view = !app.pression_view
+					app.mouse_pressed = false}
+				(app.mouse_y > 56 && app.mouse_y < 76){app.carre_circle = !app.carre_circle
+					app.mouse_pressed = false}
+				(app.mouse_y > 86 && app.mouse_y < 106){app.substeps -= 1.0
+					app.mouse_pressed = false}
+				(app.mouse_y > 116 && app.mouse_y < 136){app.list_parti = []
+					app.mouse_pressed = false}
+				(app.mouse_y > 146 && app.mouse_y < 166){app.red_factor -= 1
+					app.mouse_pressed = false}
+				(app.mouse_y > 176 && app.mouse_y < 196){app.green_factor -= 1
+					app.mouse_pressed = false}
+				(app.mouse_y > 206 && app.mouse_y < 226){app.blue_factor -= 1
+					app.mouse_pressed = false}
+				(app.mouse_y > 236 && app.mouse_y < 256){app.min_parti_size -= 1
+					app.mouse_pressed = false}
+				(app.mouse_y > 266 && app.mouse_y < 286){app.max_parti_size -= 1
+					app.mouse_pressed = false}
+                else{}
+            }
+        }else if app.mouse_x > 1070{
+            match true{
+                (app.mouse_y > 86 && app.mouse_y < 106){app.substeps += 1.0
+					app.mouse_pressed = false}
+				(app.mouse_y > 146 && app.mouse_y < 166){app.red_factor += 1
+					app.mouse_pressed = false}
+				(app.mouse_y > 176 && app.mouse_y < 196){app.green_factor += 1
+					app.mouse_pressed = false}
+				(app.mouse_y > 206 && app.mouse_y < 226){app.blue_factor += 1
+					app.mouse_pressed = false}
+				(app.mouse_y > 236 && app.mouse_y < 256){app.min_parti_size += 1
+					app.mouse_pressed = false}
+				(app.mouse_y > 266 && app.mouse_y < 286){app.max_parti_size += 1
+					app.mouse_pressed = false}
+                else{}
+            }
+        }
+    }
 }
